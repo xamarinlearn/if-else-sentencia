38e7ebb29c25fbe20c9f07e0653ea5cbb0271002
jese.alvarado <jese.alvarado@CDS3UNIVO3>
2019-07-19 08:33:24 -0600

    Funcionalidad agregada

--- src/ejercicio1/NumberApp/NumberApp/NumberApp/MainPage.xaml.cs
+++ src/ejercicio1/NumberApp/NumberApp/NumberApp/MainPage.xaml.cs
@@ -12,6 +12,18 @@ namespace NumberApp
         public MainPage()
         {
             InitializeComponent();
+
+            Random random = new Random();
+           int numero = random.Next();
+
+            if (numero > 10)
+            {
+                System.Diagnostics.Debug.WriteLine("Número es mayor de 10...");
+            }
+            else
+            {
+                System.Diagnostics.Debug.WriteLine("Número no es mayor de 10...");
+            }
         }
     }
 }
